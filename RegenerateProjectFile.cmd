7256ddd8f4a491b77c7ce744b4d615602afb45fd
CCP Aporia <28982391+ccp-aporia@users.noreply.github.com>
2020-05-15 17:31:16 +0000

    Merge using //eve/development/MAINLINE->//eve/sandbox/PER-BRANCH-SDK-FOLDER
    This brings in the vendored SDK work
    https://jira.ccpgames.com/browse/TTC-1025
    
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/carbon/src/": change = 1727524]

--- RegenerateProjectFile.cmd
+++ RegenerateProjectFile.cmd
@@ -1,9 +1,7 @@
 @echo off
 echo Checking out project and filters file
-p4 edit ImageIO.vcxproj
-p4 edit ImageIO.vcxproj.filters
-p4 edit ImageIO.orbis.vcxproj
-p4 edit ImageIO.orbis.vcxproj.filters
+p4 edit ImageIO_v141.vcxproj
+p4 edit ImageIO_v141.vcxproj.filters
 echo Regenerating
-..\..\..\..\..\..\shared_tools\python\27\python.exe ..\..\tools\ProjectFileGenerator\ProjectFileGenerator.py -i ImageIO.ccpproj
+..\..\..\..\..\..\shared_tools\python\27\python.exe ..\..\tools\ProjectFileGenerator\ProjectFileGenerator.py -i ImageIO.ccpproj --outfile=ImageIO_v141.vcxproj --toolset=v141
 pause
--- tests/RegenerateProjectFile.cmd
+++ tests/RegenerateProjectFile.cmd
@@ -1,7 +1,7 @@
 @echo off
 echo Checking out project and filters file
-p4 edit ImageIOTest.vcxproj
-p4 edit ImageIOTest.vcxproj.filters
+p4 edit ImageIOTest_v141.vcxproj
+p4 edit ImageIOTest_v141.vcxproj.filters
 echo Regenerating
-..\..\..\..\..\..\shared_tools\python\27\python.exe ..\..\tools\ProjectFileGenerator\ProjectFileGenerator.py -i ImageIOTest.ccpproj
+..\..\..\..\..\..\shared_tools\python\27\python.exe ..\..\tools\ProjectFileGenerator\ProjectFileGenerator.py -i ImageIOTest.ccpproj --outfile=ImageIOTest_v141.vcxproj --toolset=v141
 pause
